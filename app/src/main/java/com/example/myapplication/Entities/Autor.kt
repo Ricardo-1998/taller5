71e34c777a82a9ae1522cbfde984b11bebc5450d
feranstirman1 <00024117@uca.edu.sv>
2019-05-22 15:27:39 -0600

    tabla autor ahora tiene primary key

--- app/src/main/java/com/example/myapplication/Entities/Autor.kt
+++ app/src/main/java/com/example/myapplication/Entities/Autor.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication.Entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
 @Entity(tableName = "author_table")
 data class Autor(
     val nombre:String,
     val fechaNacimiento:Date
-)
+){
+    @PrimaryKey(autoGenerate = true)
+    val id:Long = 0
+
+}
