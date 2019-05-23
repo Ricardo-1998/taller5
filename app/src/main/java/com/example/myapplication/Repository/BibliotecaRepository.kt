2cec14de8c427826d63340df3b96c3d28f9ce5c0
Ricardo-1998 <00004817@uca.edu.sv>
2019-05-23 11:30:37 -0600

    se pusieron bien los nombres y cambio el repositorio a uno solo

--- app/src/main/java/com/example/myapplication/Repository/BibliotecaRepository.kt
+++ app/src/main/java/com/example/myapplication/Repository/BibliotecaRepository.kt
@@ -2,27 +2,32 @@ package com.example.myapplication.Repository
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import com.example.myapplication.Database.AutorDao
 import com.example.myapplication.Database.LibroDao
+import com.example.myapplication.Entities.Autor
 import com.example.myapplication.Entities.Libro
 
-class BookRepository(private val libroDao : LibroDao) {
-
+class BibliotecaRepository(private val autorDao : AutorDao, private val libroDao : LibroDao){
 
     //LIBRO DAO METODOS
     fun allBooks() : LiveData<List<Libro>> = libroDao.getAllBooks()
 
     @WorkerThread
     suspend fun insertBook(libro : Libro){
-        libroDao.insertBook(libro)
+        libroDao.insert(libro)
     }
 
-    fun deleteLibro() = libroDao.deleteLibro()
+    //fun deleteLibro() = libroDao.deleteLibro()
 
     @WorkerThread
     fun buscarLibro(referencia : String)=  libroDao.buscarLibro(referencia)
 
-    @WorkerThread
-    fun buscarUnLibro(isbn : String)= libroDao.buscarLibro(isbn)
-    //----------------------------------------------------------------------
+    //AUTOR DAO METODOS
 
+    fun getAllAuthors() : LiveData<List<Autor>> = autorDao.getAllAuthors()
+
+    @WorkerThread
+    suspend fun insertAuthor(autor : Autor){
+        autorDao.insert(autor)
+    }
 }
--- app/src/main/java/com/example/myapplication/ViewModels/BibliotecaViewModel.kt
+++ app/src/main/java/com/example/myapplication/ViewModels/BibliotecaViewModel.kt
@@ -4,41 +4,41 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.Database.LibraryDatabase
 import com.example.myapplication.Entities.Autor
 import com.example.myapplication.Entities.Libro
-import com.example.myapplication.Repository.AuthorRepository
-import com.example.myapplication.Repository.BookRepository
+import com.example.myapplication.Repository.BibliotecaRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class BibliotecaViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val bookRepository:  BookRepository
-    private val authorRepository:  AuthorRepository
+    private val bibliotecaRepository: BibliotecaRepository
+
 
     init {
-        val repoDao = RoomDB.getInstance(application).repoDao()
-        bookRepository = BookRepository(repoDao)
-        authorRepository = AuthorRepository(repoDao)
+        val bookDao = LibraryDatabase.getInstance(application).libroDao()
+        val authorDao = LibraryDatabase.getInstance(application).autorDao()
+
+        bibliotecaRepository = BibliotecaRepository(authorDao,bookDao)
     }
 
-    fun allBooks() : LiveData<List<Libro>> = bookRepository.allBooks()
+    fun allBooks() : LiveData<List<Libro>> = bibliotecaRepository.allBooks()
 
-    fun insertBook(libro : Libro) = viewModelScope.launch(Dispatchers.IO){bookRepository.insertBook(libro)}
+    fun insertBook(libro : Libro) = viewModelScope.launch(Dispatchers.IO){bibliotecaRepository.insertBook(libro)}
 
-    fun deleteBook() =  bookRepository.deleteLibro()
+    //fun deleteBook() =  bibliotecaRepository.deleteLibro()
 
-    fun buscarLibro(referencia : String) = viewModelScope.launch(Dispatchers.IO){bookRepository.buscarLibro(referencia)}
-
-    fun buscarUnLibro(isbn : String) = viewModelScope.launch(Dispatchers.IO){bookRepository.buscarUnLibro(isbn)}
+    fun buscarLibro(referencia : String) = viewModelScope.launch(Dispatchers.IO){bibliotecaRepository.buscarLibro(referencia)}
 
 
 
-    fun getAllAuthors() : LiveData<List<Autor>> = authorRepository.getAllAuthors()
 
-    fun insertAuthor(autor: Autor) = viewModelScope.launch(Dispatchers.IO) {authorRepository.insertAuthor(autor)}
+    fun getAllAuthors() : LiveData<List<Autor>> = bibliotecaRepository.getAllAuthors()
 
-    fun deleteAuthors() = authorRepository.deleteAuthors()
+    fun insertAuthor(autor: Autor) = viewModelScope.launch(Dispatchers.IO) {bibliotecaRepository.insertAuthor(autor)}
+
+    //fun deleteAuthors() = authorRepository.deleteAuthors()
 
 
 
